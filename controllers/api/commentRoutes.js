5fbb39285bee23398c4225683095c5f9cbe6b779
astrange90 <astrange90@hotmail.com>
2024-01-24 19:25:06 -0800

    created the comment table, the association, and the comment API routes

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newcomment = await Comment.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.json(newcomment);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const commentData = await Comment.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!commentData) {
+      res.status(404).json({ message: 'No comment found with this id!' });
+      return;
+    }
+
+    res.json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
