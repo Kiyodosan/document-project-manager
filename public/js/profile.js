64ed553a4ac74569ab2eae42f0fa326a0e184d83
Kiyodo <tyler.odo@gmail.com>
2024-01-26 13:16:40 -0800

    moved jQuery CDN from profile.handlebars file to main.handlebars file + added uploaded image display in profile.handlebars and post.handlebars files + added image preview functionality to upload-widget.js file + fixed if statement for conditional rendering in profile.handlebars file + added bg-image class to body for potential styling in main.handlebars file

--- public/js/profile.js
+++ public/js/profile.js
@@ -3,21 +3,45 @@ const newFormHandler = async (event) => {
 
   const title = document.querySelector('#post-title').value.trim();
   const content = document.querySelector('#post-content').value.trim();
+  const fileData = document.querySelector('#fileData');
+  const file_url = fileData.value;
 
   if (title && content) {
-    const response = await fetch(`/api/post`, {
-      method: 'POST',
-      body: JSON.stringify({ title, content }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+    if (fileData) {
+      const response = await fetch(`/api/post`, {
+        method: 'POST',
+        body: JSON.stringify({ title, content, file_url }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
 
-    if (response.ok) {
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('An error has occured while creating the post');
+      }
+    } else {
+      const response = await fetch(`/api/post`, {
+        method: 'POST',
+        body: JSON.stringify({ title, content }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('An error has occured while creating the post');
+      }
+    }
+
+/*     if (response.ok) {
       document.location.replace('/profile');
     } else {
       alert('An error has occured while creating the post');
-    }
+    } */
   }
 };
 
