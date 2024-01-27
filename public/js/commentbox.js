355b231ccc9b4e20e79feaf7b6d80d40eb38e2ab
astrange90 <astrange90@hotmail.com>
2024-01-27 10:01:13 -0800

    removed post.handlebars

--- public/js/commentbox.js
+++ public/js/commentbox.js
@@ -8,7 +8,7 @@ const commentFormHandler = async (event) => {
     if (comment) {
       const response = await fetch('/api/comment', {
         method: 'POST',
-        body: JSON.stringify({ content:comment }),
+        body: JSON.stringify({ comment }),
         headers: {
           'Content-Type': 'application/json',
         },
