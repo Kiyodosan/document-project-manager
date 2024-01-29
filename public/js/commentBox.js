55effdb5ba54d9d51d4fabb0fa852e93eff9f884
Tim Van Deusen <145621518+timvandeusen@users.noreply.github.com>
2024-01-28 16:42:24 -0800

    fixed delete button for posts

--- public/js/commentBox.js
+++ public/js/commentBox.js
@@ -22,35 +22,20 @@ const commentFormHandler = async (event) => {
 };
 
 const deleteFormHandler = async (event) => {
-  if (event.target.matches('.delete-comment')) {
-    const commentId = event.target.getAttribute('data-id');
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
 
-    try {
-      const response = await fetch(`/api/comment/${commentId}`, {
-        method: 'DELETE',
-      });
+    const response = await fetch(`/api/comment/${id}`, {
+      method: 'DELETE',
+    });
 
-      if (response.ok) {
-        document.location.reload();
-      } else {
-        console.error('Error deleting comment:', response.statusText);
-      }
-    } catch (error) {
-      console.error('Unexpected error:', error);
+    if (response.ok) {
+      document.location.replace('/post/' + post_id);
+    } else {
+      alert('An error has occured while deleting the comment');
     }
   }
 };
 
-document.body.addEventListener('click', async function (event) {
-  console.log('Body clicked!');
-  if (event.target.hasAttribute('data-id')) {
-    await deleteFormHandler(event);
-  }
-});
-
 document.querySelector('#send-comment').addEventListener('click', commentFormHandler);
-document.body.addEventListener('click', async function (event) {
-  if (event.target.hasAttribute('data-id')) {
-    await deleteFormHandler(event);
-  }
-});
+document.querySelector('#delete-comment').addEventListener('click', deleteFormHandler);
